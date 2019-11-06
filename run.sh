f24106415313b22fd54d1b759251c01b81ae2e19
Jan Kuester <jkuester@uni-bremen.de>
2019-11-06 16:30:45 +0100

    update ports

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 meteor npm install
 
 PACKAGE_DIRS="../lib:../liboauth"
-METEOR_PACKAGE_DIRS=${PACKAGE_DIRS} meteor --port=8080 --settings=settings.json
+METEOR_PACKAGE_DIRS=${PACKAGE_DIRS} meteor --port=9090 --settings=settings.json
