bb0cf51b60e96a08119259ec7e61447127d7c17c
Jan Kuester <jkuester@uni-bremen.de>
2019-11-29 13:21:00 +0100

    create accounts from settings file

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,9 +1,20 @@
 import { Meteor } from 'meteor/meteor'
 import { Accounts } from 'meteor/accounts-base'
 
+const users = Meteor.settings.users
+
 Meteor.startup(() => {
-  if (Meteor.users.find().count() === 0) {
-    const userId = Accounts.createUser({ username: 'admin', password: 'password' })
-    console.info(`DEFAULT: user created with id ${userId} - login via admin / password and change username and password`)
-  }
+  users.forEach(user => {
+    const username = user.username
+    if (Meteor.users.findOne({ username })) {
+      // skip this user as she already exists
+      return
+    }
+
+    const password = user.password
+    const userId = Accounts.createUser({ username, password })
+    if (Meteor.isDevelopment) {
+      console.info(`DEFAULT: user created with id ${userId} - login via ${username}`)
+    }
+  })
 })
