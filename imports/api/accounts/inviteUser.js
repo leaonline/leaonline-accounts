d00727f71a6c6c515f53e9c6a4b606984f018304
Jan Küster <jkuester@uni-bremen.de>
2020-03-31 11:04:20 +0200

    api inviteUser attach persona to user document

--- imports/api/accounts/inviteUser.js
+++ imports/api/accounts/inviteUser.js
@@ -4,14 +4,14 @@ import { Accounts } from 'meteor/accounts-base'
 import { Random } from 'meteor/random'
 import { check } from 'meteor/check'
 
-const createUser = ({ email, username, firstName, lastName, institution }) => {
+const createUser = ({ email, username, firstName, persona, lastName, institution }) => {
   if (Accounts.findUserByEmail(email) || Accounts.findUserByUsername(username)) {
     throw new Meteor.Error('createUser.userExists')
   }
 
   const password = Random.id(32)
   const userId = Accounts.createUser({ username, email, password })
-  const updated = Meteor.users.update(userId, { $set: { firstName, lastName, institution } })
+  const updated = Meteor.users.update(userId, { $set: { firstName, lastName, institution, persona } })
 
   if (!updated) {
     throw new Meteor.Error('createUser.updateFailed')
@@ -45,7 +45,7 @@ export const inviteUser = ({ email, username, persona, firstName, lastName, inst
   check(lastName, String)
   check(institution, String)
 
-  const userId = createUser({ email, username, firstName, lastName, institution })
+  const userId = createUser({ email, username, firstName, lastName, persona, institution })
   assignRole(userId, persona, institution)
   sendEnrollmentEmail(userId)
   return userId
