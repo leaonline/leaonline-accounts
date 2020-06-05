bc9ba463cd9f03bdbabf77ab20b543bf418e2c0c
Jan Küster <jkuester@uni-bremen.de>
2020-06-05 17:46:05 +0200

    startup more verbose startup info

--- imports/startup/server/accounts/users.js
+++ imports/startup/server/accounts/users.js
@@ -5,16 +5,20 @@ import { inviteUser } from '../../../api/accounts/inviteUser'
 const users = Meteor.settings.accounts.users
 
 Meteor.startup(() => {
+  console.info('DEFAULT: check accounts')
   users.forEach(user => {
     const { email } = user
     if (Accounts.findUserByEmail(email)) {
       // skip this user as she already exists
+      console.info(`DEFAULT: User exists for mail ${email}`)
       return
     }
-
-    const userId = inviteUser(user)
-    if (Meteor.isDevelopment) {
+    console.info(`DEFAULT: invite user with email ${email}`)
+    try {
+      const userId = inviteUser(user)
       console.info(`DEFAULT: user created with id ${userId} - login via ${user.email}`)
+    } catch(error) {
+      console.error(error)
     }
   })
 })
--- imports/startup/server/roles.js
+++ imports/startup/server/roles.js
@@ -1,11 +1,15 @@
-/* global Roles */
+import { Roles } from 'meteor/alanning:roles'
 import { check } from 'meteor/check'
 import { Meteor } from 'meteor/meteor'
+
 const { clients } = Meteor.settings.oauth
 
 const createRole = name => {
   check(name, String)
-  if (Meteor.roles.findOne(name)) return
+  if (Meteor.roles.findOne(name)) {
+    return console.info(`[Roles]: top-level role ${name} already exists`)
+  }
+
   Roles.createRole(name)
   console.info(`[Roles]: created top-level role ${name}`)
 }
