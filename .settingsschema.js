ce64ae51382bdfa17c9df9efa7219d935eac72ee
Jan Küster <jkuester@uni-bremen.de>
2020-10-23 11:20:09 +0200

    moved oauth server settings into settings file

--- .settingsschema.js
+++ .settingsschema.js
@@ -1,27 +1,21 @@
 const SimpleSchema = require('simpl-schema')
 const oauthFlows = ['authorization_code']
-const rlativeUrl = /\/[a-zA-Z\/-]+/
 const schema = def => new SimpleSchema(def)
+
+const relativeUrl = {
+  type: String,
+  regEx: /\/[a-zA-Z\/-]+/
+}
 
 const settingsSchema = schema({
   public: schema({
     oauth: schema({
-      dialogUrl: {
-        type: String,
-        regEx: rlativeUrl
-      },
-      accessTokenUrl: {
-        type: String,
-        regEx: rlativeUrl
-      },
-      authorizeUrl: {
-        type: String,
-        regEx: rlativeUrl
-      },
-      identityUrl: {
-        type: String,
-        regEx: rlativeUrl
-      }
+      dialogUrl: relativeUrl,
+      accessTokenUrl: relativeUrl,
+      authorizeUrl: relativeUrl,
+      identityUrl: relativeUrl,
+      errorUrl: relativeUrl,
+      fallbackUrl: relativeUrl
     })
   }),
   accounts: schema({
@@ -60,34 +54,54 @@ const settingsSchema = schema({
       'roles.$': String
     })
   }),
-  oauth: Object,
-  'oauth.clients': Array,
-  'oauth.clients.$': schema({
-    key: String,
-    title: String,
-    description: String,
-    clientId: {
-      type: String,
-      regEx: SimpleSchema.RegEx.idOfLength(16, null)
-    },
-    secret: {
-      type: String,
-      regEx: SimpleSchema.RegEx.idOfLength(32, null)
-    },
-    url: {
-      type: String,
-      regEx: SimpleSchema.RegEx.WeakUrl
-    },
-    redirectUris: Array,
-    'redirectUris.$': {
-      type: String,
-      regEx: SimpleSchema.RegEx.WeakUrl
-    },
-    grants: Array,
-    'grants.$': {
-      type: String,
-      allowedValues: oauthFlows
-    }
+  oauth: schema({
+    debug: Boolean,
+    clients: Array,
+    'clients.$': schema({
+      key: String,
+      title: String,
+      description: String,
+      clientId: {
+        type: String,
+        regEx: SimpleSchema.RegEx.idOfLength(16, null)
+      },
+      secret: {
+        type: String,
+        regEx: SimpleSchema.RegEx.idOfLength(32, null)
+      },
+      url: {
+        type: String,
+        regEx: SimpleSchema.RegEx.WeakUrl
+      },
+      redirectUris: Array,
+      'redirectUris.$': {
+        type: String,
+        regEx: SimpleSchema.RegEx.WeakUrl
+      },
+      grants: Array,
+      'grants.$': {
+        type: String,
+        allowedValues: oauthFlows
+      }
+    }),
+    server: schema({
+      addAcceptedScopesHeader: Boolean,
+      addAuthorizedScopesHeader: Boolean,
+      allowBearerTokensInQueryString: Boolean,
+      allowEmptyState: Boolean,
+      authorizationCodeLifetime: SimpleSchema.Integer,
+      accessTokenLifetime: SimpleSchema.Integer,
+      refreshTokenLifetime: SimpleSchema.Integer,
+      allowExtendedTokenAttributes: Boolean,
+      requireClientAuthentication: Boolean
+    }),
+    model: schema({
+      debug: Boolean,
+      accessTokensCollectionName: String,
+      refreshTokensCollectionName: String,
+      clientsCollectionName: String,
+      authCodesCollectionName: String
+    })
   })
 })
 
--- imports/startup/server/oauth.js
+++ imports/startup/server/oauth.js
@@ -2,53 +2,37 @@ import { Meteor } from 'meteor/meteor'
 import { OAuth2Server } from 'meteor/leaonline:oauth2-server'
 import { canUserAccessClient } from '../../api/accounts/canUserAccessClient'
 
-const { clients } = Meteor.settings.oauth
-const urls = Meteor.settings.public.oauth
-
+const { clients, server, model, debug } = Meteor.settings.oauth
+const routes = Meteor.settings.public.oauth
 const oauth2server = new OAuth2Server({
-  serverOptions: {
-    addAcceptedScopesHeader: true,
-    addAuthorizedScopesHeader: true,
-    allowBearerTokensInQueryString: false,
-    allowEmptyState: false,
-    authorizationCodeLifetime: 300,
-    accessTokenLifetime: 3600,
-    refreshTokenLifetime: 1209600,
-    allowExtendedTokenAttributes: false,
-    requireClientAuthentication: true
-  },
-  model: {
-    accessTokensCollectionName: 'oauth_access_tokens',
-    refreshTokensCollectionName: 'oauth_refresh_tokens',
-    clientsCollectionName: 'oauth_clients',
-    authCodesCollectionName: 'oauth_auth_codes',
-    debug: false
-  },
-  routes: {
-    accessTokenUrl: urls.accessTokenUrl,
-    authorizeUrl: urls.authorizeUrl,
-    errorUrl: '/oauth/error',
-    fallbackUrl: '/oauth/*'
-  },
+  serverOptions: server,
+  model: model,
+  routes: routes,
   debug: true
 })
 
-OAuth2Server.validateUser(canUserAccessClient)
+oauth2server.validateUser(canUserAccessClient)
 
-oauth2server.authenticatedRoute().get(urls.identityUrl, function (req, res, next) {
+oauth2server.authenticatedRoute().get(routes.identityUrl, function (req, res, next) {
   const user = Meteor.users.findOne(req.data.user.id)
+
+  // TODO fail with 404 if user is not found
 
   res.writeHead(200, {
     'Content-Type': 'application/json'
   })
-  const body = JSON.stringify({
-    id: user._id,
-    login: user.username,
-    email: user.emails && user.emails[0].address,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    name: `${user.firstName} ${user.lastName}`
-  })
+
+  const body = user
+    ? JSON.stringify({
+      id: user._id,
+      login: user.username,
+      email: user.emails && user.emails[0]?.address,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      name: `${user.firstName} ${user.lastName}`
+    })
+    : ''
+
   res.end(body)
 })
 
