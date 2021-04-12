105b0c55a108cf4085603442e3b7ec104aee704c
Jan Küster <jkuester@uni-bremen.de>
2021-04-12 13:17:16 +0200

    tests updated package dirs for ci

--- test.sh
+++ test.sh
@@ -49,6 +49,7 @@ then
     PROJECT_NAME=`basename "$PROJECT_ROOT"`
 	echo "=> Test: $PROJECT_NAME"
 	echo "=> Path: [${PROJECT_ROOT}]"
+	echo "=> Patent Path: [${PARENT_ROOT}]"
 	echo "=> Port: [${PORT}]"
 	echo "=> Lib path(s): [${PACKAGE_DIRS}]"
 	echo "=> Watch mode: [${WATCH_MODE}] ${RUN_ONCE}"
