ea029dc92acee2ab21329d3c7e1114aeecd5334d
Jan Küster <jkuester@uni-bremen.de>
2021-04-13 12:16:50 +0200

    scripts lint.sh added -f flag to run fix mode

--- lint.sh
+++ lint.sh
@@ -2,6 +2,26 @@
 
 set -e
 
-meteor npm run lint:code
-meteor npm run lint:style
+while getopts "f" opt; do
+  case $opt in
+    f)
+	  FIX_MODE=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "FIX_MODE" -eq "0" ];
+then
+    meteor npm run lint:code
+    meteor npm run lint:style
+    else
+    meteor npm run lint:code-fix
+    meteor npm run lint:style-fix
+fi
+
+
 meteor npm run lint:markdown
