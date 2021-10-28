d2c7620fc2bdc7553ffabd9f8c3d8e8698553ea7
Jan Küster <jkuester@uni-bremen.de>
2021-10-28 07:23:44 +0200

    upgrade to meteor 2.5

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 meteor npm install
 
-PACKAGE_DIRS="../lib:../liboauth:../meteor-autoform-themes"
-METEOR_PACKAGE_DIRS=${PACKAGE_DIRS} meteor --port=9090 --settings=settings.json
+PACKAGE_DIRS="../lib:../liboauth:../libext:../meteor-autoform-themes"
+METEOR_PACKAGE_DIRS=${PACKAGE_DIRS} meteor --port=9090 --settings=settings.json --allow-incompatible-update --exclude-archs "web.browser.legacy, web.cordova"
