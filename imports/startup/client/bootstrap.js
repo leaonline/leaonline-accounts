e309ad8323dee1f9e87e43ecd20b93482ebc1535
jankapunkt <jkuester@uni-bremen.de>
2024-07-29 11:39:46 +0200

    migration: upgrade to 3.0 completed

--- imports/startup/client/bootstrap.js
+++ imports/startup/client/bootstrap.js
@@ -2,9 +2,8 @@
 import { Meteor } from 'meteor/meteor'
 import '@fortawesome/fontawesome-free/js/all'
 import 'bootstrap'
-import popper from 'popper.js'
-import autoformBs4 from 'meteor/communitypackages:autoform-bootstrap4'
-import { AutoFormThemeBootstrap4 } from 'meteor/communitypackages:autoform-bootstrap4/dynamic'
+import popper from '@popperjs/core'
+import { AutoFormThemeBootstrap5 } from 'meteor/communitypackages:autoform-bootstrap5/dynamic'
 import { AutoFormPassword2 } from 'meteor/jkuester:autoform-password2/dynamic'
 import 'meteor/aldeed:autoform/dynamic'
 import './theme.scss'
@@ -13,12 +12,12 @@ global.Popper = global.Popper || popper
 
 async function init () {
   await AutoForm.load()
-  await AutoFormThemeBootstrap4.load()
+  await AutoFormThemeBootstrap5.load()
   await AutoFormPassword2.load()
-  AutoForm.setDefaultTemplate('bootstrap4')
+  AutoForm.setDefaultTemplate('bootstrap5')
 }
 
-Meteor.startup(() => {
-  init()
-  console.info({ autoformBs4 })
+Meteor.startup(async () => {
+  await init()
+  console.info({ AutoFormThemeBootstrap5 })
 })
